f53d79b4c22523d21a2bf123cf0fb69476aa9dcf
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-29 22:59:52 +0100

    Temporary resolve typescript issues for first deployment

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -2,16 +2,20 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import MultiSelect from "react-multi-select-component";
-import React from "react";
-import Image from "next/image";
+import React, { forwardRef, Ref, HTMLAttributes } from "react";
+import Image, { ImageProps } from "next/image";
 
 export function cn(...inputs: (string | boolean | undefined)[]) {
   return twMerge(clsx(inputs));
 }
 
 // MultiSelect Setup
-//@ts-ignore
-const MultiSelectComponent = React.forwardRef(({ options, ...rest }, ref) => {
+const MultiSelectComponent = forwardRef<
+  HTMLDivElement,
+  {
+    options: any[];
+  }
+>(({ options, ...rest }, ref) => {
   return (
     <>
       {/* @ts-ignore */}
@@ -21,16 +25,23 @@ const MultiSelectComponent = React.forwardRef(({ options, ...rest }, ref) => {
 });
 
 // Helper component to handle image hover effect
-//@ts-ignore
-const GrayToColorImage = ({ img, className }) => {
+interface GrayToColorImageProps {
+  img: string;
+  className?: string;
+}
+
+const GrayToColorImage: React.FC<GrayToColorImageProps & ImageProps> = ({
+  img,
+  className,
+  ...props
+}) => {
   return (
     <div className={`relative ${className}`}>
       <Image
-        src={img}
         fill
         style={{ objectFit: "cover" }}
         className="filter grayscale hover:grayscale-0 cursor-pointer transition duration-300"
-        alt="icon"
+        {...props}
       />
     </div>
   );
