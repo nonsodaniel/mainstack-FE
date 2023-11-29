135e9ef7d5755e18f62061b826c3da003149d959
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-29 01:56:06 +0100

    Created fetch hook

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+const URL = `https://fe-task-api.mainstack.io/`;
+export const userURL = URL + "user";
+export const walletURL = URL + "wallet";
+export const transactionURL = URL + "transactions";
--- utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const sidebarLinks = [];
-
-export const tabs = [];
