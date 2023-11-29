dfc2a3b7e52957c2f92342d56b728eb49bd3f170
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-29 19:19:51 +0100

    Remove unused files

--- app/parts/Analytics.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Analytics = () => {
-  return (
-    <div className="w-full lg:w-[80%] mx-auto pt-32">Component Here!!</div>
-  );
-};
-
-export default Analytics;
